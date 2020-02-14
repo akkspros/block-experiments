77713eedcc9807d465d687ec85691221223b8372
Alex Lende <ajlende@gmail.com>
2020-02-14 12:21:05 +0000

    Rename jetpack to a8c

--- blocks/event/index.php
+++ blocks/event/index.php
@@ -1,7 +1,7 @@
 <?php
 
 add_action( 'init', function() {
-	register_block_type( 'jetpack/event', [
+	register_block_type( 'a8c/event', [
 		'editor_script' => 'wpcom-blocks',
 		'style' => 'wpcom-blocks',
 		'editor_style' => 'wpcom-blocks-editor',
